43642f6f6167ce419684fd740fa8fb1b6fc70dac
Aaron Howe <75334813+aaronmhowe@users.noreply.github.com>
2024-07-18 01:35:50 -0700

    wrote skeleton code for the main class

---
+++ src/main.py
@@ -0,0 +1,45 @@
+class SummitSelect_Main:
+
+
+    def __init__(self, run_count_data, price_data, elevation_data):
+
+        return 0
+    
+
+    def process_data(self):
+
+        return 0
+    
+
+    def create_rankings(self, data):
+
+        return 0
+    
+
+    def create_final_ranking(self, rankings):
+
+        return 0
+    
+
+    def dump_output(self, rankings, final_ranking):
+
+        return 0
+    
+
+    def run(self):
+
+        return 0:
+    
+
+def add_args():
+
+    return 0
+
+
+def main():
+
+    return 0
+
+
+def __name__ == "__main__":
+    main()
