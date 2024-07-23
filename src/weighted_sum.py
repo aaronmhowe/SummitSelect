fe5752c39bab36192e8a38de23c29cd617930200
Aaron Howe <75334813+aaronmhowe@users.noreply.github.com>
2024-07-22 17:34:26 -0700

    pushed major bug fixes for normalizing data and receiving user preferences
    Results from data normalization were computing incorrectly, values came out negative when they should've been positive.

--- src/weighted_sum.py
+++ src/weighted_sum.py
@@ -19,15 +19,18 @@ class WeightedSumModel:
     @param data: ranked ski resort data acquired from process_data.py
     """
     def __init__(self, data: pd.DataFrame):
+
+        self.preferences = {'Run Count': True, 'Price (USD)': True, 'Peak Elevation (m)': True}
         
-        data_columns = ['Resort ID', 'Resort', 'Country', 'Run Count', 'Price (USD)', 'Peak Elevation (m)']
-        missing_cols = set(data_columns) - set(data.columns)
+        # data_columns = ['Resort ID', 'Resort', 'Country', 'Run Count', 'Price (USD)', 'Peak Elevation (m)']
+        # missing_cols = set(data_columns) - set(data.columns)
 
-        if missing_cols:
+        # if missing_cols:
 
-            raise ValueError(f"Missing input data: {', '.join(missing_cols)}")
+        #     raise ValueError(f"Missing input data: {', '.join(missing_cols)}")
         
         self.data = data.copy()
+        # self.original_data = data.copy()
         self.weights = {'Run Count': 0.33, 'Price (USD)': 0.33, 'Peak Elevation (m)': 0.33}
         self.normalized = pd.DataFrame()
         self.w_scores = pd.DataFrame()
@@ -52,21 +55,17 @@ class WeightedSumModel:
         for feature in features:
 
             while True:
-
                 response = input(f"{feature} Preference (Yes/No): ").strip().lower()
 
                 if response in ['y', 'yes']:
-
                     user_preference[feature] = True
                     break
 
                 elif response in ['n', 'no']:
-
                     user_preference[feature] = False
                     break
 
                 else:
-
                     print("Input Error: Please Enter a 'Yes' or 'No' Response...")
 
         feature_weights = 1.0 / len(features)
@@ -75,7 +74,6 @@ class WeightedSumModel:
         print("\nCalculated Weights:")
         
         for feature, weight in self.weights.items():
-
             print(f"{feature}: {weight:.2f}")
 
         print("\nPositive Values = Higher Run Count and Elevation, and Lower Prices...")
@@ -87,16 +85,13 @@ class WeightedSumModel:
     def normalize_data(self) -> None:
 
         if self.data.empty:
-
             raise ValueError("Data Not Found...")
         
-        normalized_features = ['Run Count', 'Price (USD)', 'Peak Elevation (m)']
         self.normalized = self.data[['Resort ID', 'Resort', 'Country']].copy()
 
-        for feature in normalized_features:
+        for feature in ['Run Count', 'Price (USD)', 'Peak Elevation (m)']:
 
             if feature not in self.data.columns:
-
                 raise ValueError(f"Couldn't Find '{feature}' in the Data-Set(s)")
             
             min = self.data[feature].min()
@@ -104,21 +99,25 @@ class WeightedSumModel:
 
             # setting the normalized score to 1 if the values of the features are equal
             if min == max:
-
                 self.normalized[f'{feature} (Normalized)'] = 1
 
             else:
 
                 if feature == 'Price (USD)':
-
                     self.normalized[f'{feature} (Normalized)'] = (max - self.data[feature]) / (max - min)
 
                 else:
-
                     self.normalized[f'{feature} (Normalized)'] = (self.data[feature] - min) / (max - min)
 
-        print(f"Normalized Features {', '.join(normalized_features)}")
+        print("Normalized Features: Run Count, Price (USD), Peak Elevation (m)")
     
+
+    def set_preferences(self, run_pref: bool, price_pref: bool, elevation_pref: bool):
+
+        self.preferences['Run Count'] = run_pref
+        self.preferences['Price (USD)'] = price_pref
+        self.preferences['Peak Elevation (m)'] = elevation_pref
+
 
     """
     Algorithm for the weighted sum model, computing the weighted sum for each resort to construct
@@ -127,27 +126,20 @@ class WeightedSumModel:
     def weighted_sum_model(self) -> None:
 
         if self.normalized.empty:
-
             raise ValueError("Features Not Normalized...")
         
         if not self.weights:
-
             raise ValueError("No Feature Weights Found...")
         
-        self.w_scores = self.normalized[['Resort ID', 'Resort', 'Country']].copy()
-
+        self.w_scores = self.data[['Resort ID', 'Resort', 'Country', 'Run Count', 'Price (USD)', 'Peak Elevation (m)']].copy()
         overall_weight = 0
 
         # weighted_sum computation
-        for feature, weight in self.weights.items():
-
-            normalized_features = f'{feature} (Normalized)'
-
-            if normalized_features not in self.normalized.columns:
-
-                raise ValueError(f"Could Not Find Normalized Feature(s): {feature}")
+        for feature, preference in self.preferences.items():
+            weight = self.weights[feature] if preference else -self.weights[feature]
+            normalized_feature = f'{feature} (Normalized)'
             
-            self.w_scores[f'{feature} (Weight)'] = self.normalized[normalized_features] * weight
+            self.w_scores[f'{feature} (Weight)'] = self.normalized[normalized_feature] * weight
             overall_weight += self.w_scores[f'{feature} (Weight)']
 
         self.w_scores['Total Weighted Score'] = overall_weight
@@ -167,11 +159,7 @@ class WeightedSumModel:
         self.final_ranking = self.w_scores.copy()
         self.final_ranking['Rank'] = self.final_ranking.index + 1
 
-        data_columns = ['Rank', 'Resort ID', 'Resort', 'Country', 'Total Weighted Score']
-
-        for feature in self.weights.keys():
-
-            data_columns.append(f'{feature} (Weight)')
+        data_columns = ['Rank', 'Resort ID', 'Resort', 'Country', 'Run Count', 'Price (USD)', 'Peak Elevation (m)', 'Total Weighted Score']
 
         self.final_ranking = self.final_ranking[data_columns]
         print(f"Top Ranked Resort: {self.final_ranking.iloc[0]['Resort']}")
@@ -187,22 +175,18 @@ class WeightedSumModel:
     def return_ranking(self, n=10) -> pd.DataFrame:
 
         if self.final_ranking.empty:
-
             raise ValueError("Could Not Find a Final Ranking of Resorts...")
         
         if not isinstance(n, int) or n < 1:
-
             raise ValueError("Resorts 'n' is found to not hold a positive value...")
         
         if n > len(self.final_ranking):
-
             print(f"Only {len(self.final_ranking)} Resorts Can be Loaded...")
             n = len(self.final_ranking)
 
         top_n = self.final_ranking.head(n)
 
         for _, resort in top_n.iterrows():
-
-            print(f"{resort['Rank']} Ranking: {resort['Resort']}")
+            print(f"{int(resort['Rank'])} Ranking: {resort['Resort']}")
 
         return top_n
